91f8f4240ae38cf0326a111f7ea32c414d826626
Eileen Ilsemann <eileenilsemanni@gmail.com>
2023-12-30 01:27:17 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -21,14 +21,30 @@ Change the contents of this page depending on the current day and time.
 */
 
 const display = document.querySelector('[data-js="display"]');
+// let currentTime = new Date().getHours();
+// let currentDay = new Date().getDay();
 
-function getGreeting() {
-  // Code here
+function getGreeting(currentTime) {
+  if (currentTime > 5 && currentTime < 13) {
+    return (display.textContent = "Good Morning"); // das return ist sehr wichtig, sonst passiert naemlich nix
+  } else if (currentTime > 12 && currentTime < 19) {
+    return (display.textContent = "Good Afternoon");
+  } else if (currentTime > 18 && currentTime < 23) {
+    return (display.textContent = "Good Evening");
+  } else {
+    return (display.textContent = "Good Night");
+  }
 }
 
-function getDayColor() {
-  // Code here
+function getDayColor(currentDay) {
+  if (currentDay === 1) {
+    document.body.style.backgroundColor = "darkgray";
+  } else if (currentDay > 1 && currentDay < 6) {
+    document.body.style.backgroundColor = "lightblue";
+  } else {
+    document.body.style.backgroundColor = "hotpink"; // bis hier hin hat noch alles problemfrei geklappt. mal schauen wie das gleich wird, wenn ich einen parameter einbaue, damit ich das ganze auch noch testen kann...
+  }
 }
 
-display.textContent = getGreeting();
-document.body.style.backgroundColor = getDayColor();
+display.textContent = getGreeting(20);
+document.body.style.backgroundColor = getDayColor(5); // oke... meine sorge war unberechtigt. hat super geklappt.
